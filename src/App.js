18e5a2e12024ba175dd9cc69e5add88d025691fa
Dom <Dom@Dom-2.local>
2023-01-06 11:13:49 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import useArticles from './repository';
 
 function App() {
+
+
+
+  const articles = useArticles();
+
+  const [formObject, setFormObject] = useState(
+    { title: 'title1', content: 'content1' }
+  );
+
+  const [selectedArticleId, setSelectedArticleId] = useState(-1);
+
+  const selectedArticle = articles.byId(selectedArticleId)?.content ||'none';
+
+  const changeHandler = function (event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    formObject[name] = value;
+    setFormObject({ ...formObject })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={'app'}>
+      <h2>React Custom Hooks</h2>
+      <ul>   
+        {articles.list().map(
+          (articles,index) => {
+            return <li key={index}
+              className={
+                (selectedArticleId === index) ? 'selected' : ''
+          }
+          onClick={(event) => setSelectedArticleId(index)}>
+            {articles.title}
+          </li>
+          }
+        )}
+      </ul>
+      <br /><span className={'bold'}>Selected Article:</span>
+      <p>{selectedArticle}</p><br />
+
+      <br />
+      <div className={'controls'}>
+        <span className={'bold'}>Controls:</span><br />
+        <button onClick={false}>Add</button>
+        <button onClick={false} disabled={false} >  Delete</button>
+        <br />
+      </div>
+
+
     </div>
   );
 }
